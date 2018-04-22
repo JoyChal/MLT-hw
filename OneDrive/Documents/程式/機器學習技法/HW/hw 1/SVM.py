02680d1ba10144b69fa348d6752fa8e6d3354ef7
JoyChal <joych201517@gmail.com>
2018-04-23 00:35:15 +0800

    Add files via upload
    0422

--- OneDrive/Documents/程式/機器學習技法/HW/hw 1/SVM.py
+++ OneDrive/Documents/程式/機器學習技法/HW/hw 1/SVM.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 from sklearn.svm import SVC
+from sklearn.cross_validation import train_test_split
 import matplotlib.pyplot as plt
 
 def readToList02(filename):
@@ -16,106 +17,154 @@ def readToList02(filename):
 # Read in training data and test data
 TrData = np.array(readToList02('features.train.txt'))
 TeData = np.array(readToList02('features.test.txt')) 
+for eachR in TeData:
+    if eachR[0] != 1:
+       eachR[0] = -1
 
 # Prob 11.
-## Set Data
+## set Data
 for eachR in TrData:
     if eachR[0] != 1:
        eachR[0] = -1
-TrY11 = TrData[:,0]
+TrY11 = TrData[:,[0]]
 TrX11 = TrData[:,[1,2]]
 C = [pow(10,-5), pow(10,-3), 0.1, 10, 1000]
-clf11 = {} # create a dictionary to store different clf (due to diff C) in prob11 -> https://stackoverflow.com/questions/5036700/how-can-you-dynamically-create-variables-via-a-while-loop
-for i in range(1, len(C)):
-    clf11[i] = SVC(C = C[i], kernel = 'linear') # set model
-    clf11[i].fit(TrX11, TrY11) # training
+clf11 = [] # create a list to store different clf (due to diff C) in prob11 -> https://stackoverflow.com/questions/5036700/how-can-you-dynamically-create-variables-via-a-while-loop
+for i in range(0, len(C)):
+    clf = SVC(C = C[i], kernel = 'linear') # set model
+    clf.fit(TrX11, TrY11) # training
+    clf11.append(clf)
 ## get W
 clf11WeiMeg = [] # store the magnitude of wieghts 
-for i in range(1, len(clf11)):
-    tmpW  = [np.linalg.norm(clf11[i].coef_)] # https://zh.wikipedia.org/wiki/%E5%90%91%E9%87%8F
+for i in range(0, len(clf11)):
+    tmpW  = np.linalg.norm(clf11[i].coef_) # https://zh.wikipedia.org/wiki/%E5%90%91%E9%87%8F
     clf11WeiMeg.append(tmpW)
 ## plot
 plt.plot(np.log10(C), clf11WeiMeg)
 plt.title("Prob 11")
+plt.xlabel("log10(C)")
+plt.ylabel("magnitude of W")
+plt.show()
 
 # Prob 12
-## Set Data
+## set Data
+TrData = np.array(readToList02('features.train.txt'))
 for eachR in TrData:
     if eachR[0] != 8:
        eachR[0] = -1
     else: eachR[0] = 1
-TrY12 = TrData[:,0]
+TrY12 = TrData[:,[0]]
 TrX12 = TrData[:,[1,2]]
-clf12 = {} # create a dictionary to store different clf (due to diff C) in prob11 -> https://stackoverflow.com/questions/5036700/how-can-you-dynamically-create-variables-via-a-while-loop
-for i in range(1, len(C)):
-    clf12[i] = SVC(C = C[i], kernel = 'poly', coef0 = 1, gamma = 1, degree = 2) # set model
-    clf12[i].fit(TrX12, TrY12) # training
+clf12 = [] # create a dictionary to store different clf (due to diff C) in prob11 -> https://stackoverflow.com/questions/5036700/how-can-you-dynamically-create-variables-via-a-while-loop
+for i in range(0, len(C)):
+    clf = SVC(C = C[i], kernel = 'poly', coef0 = 1, gamma = 1, degree = 2) # set model
+    clf.fit(TrX12, TrY12) # training
+    clf12.append(clf)
 Ein = []
 ## get Ein
-for eachClf in range(1,len(clf12)):
-    ein = [(TrY12[i] != clf12.predict(TrX12[i])) for i in range(1,len(TrData))] # a list recond every data predict result, 0 is correct
-    Ein += [float(sum(ein))/len(TrData)]
+for eachClf in range(0,len(clf12)):
+	# original method
+    '''ein = [(TrY12[i] != clf12.predict(TrX12[i])) for i in range(1,len(TrData))] # a list recond every data predict result, 0 is correct'''
+    ein = 1-clf12[eachClf].score(TrX12, TrY12)
+    Ein += [ein] # original : float(sum(ein))/len(TrData) 
 ## plot
 plt.plot(np.log10(C), Ein)
 plt.title("Prob 12")
+plt.xlabel("log10(C)")
+plt.ylabel("Ein")
+plt.show()
 
 # Prob 13
 ## get number of support 
 nSupport = []
-for i in range(1, len(clf12)):
+for i in range(0, len(clf12)):
     ns = sum(clf12[i].n_support_)
     nSupport += [ns]
 ## plot
 plt.plot(np.log10(C), nSupport)
 plt.title("Prob 13")
+plt.xlabel("log10(C)")
+plt.ylabel("number of SV")
+plt.show()
 
 # Prob14
 C2 = [pow(10,-3), pow(10,-2), 0.1, 1, 10]
-clf14 = {}
+clf14 = []
 ## train
-for i in range(1, len(C2)):
-    clf14[i] = SVC(C = C[i], kernel = 'rbf', gamma = 80)
-    clf14[i].fit(TrX11, TrY11)
+for i in range(0, len(C2)):
+    clf = SVC(C = C[i], kernel = 'rbf', gamma = 80)
+    clf.fit(TrX11, TrY11)
+    clf14.append(clf)
 anyFreeSVind = []
 ## choose a fSV (record its indice)
-for i in range(1,len(clf14)):
-    svind = clf14.support_ # indices of support vectors. (list)
-    fsvind == C2[i] # fsvind is a choosen indice of a free support vector
+for i in range(0,len(clf14)):
+    svind = clf14[i].support_ # indices of support vectors. (list)
+    fsvind = C2[i] # fsvind is a choosen indice of a free support vector
     while fsvind == C2[i]: # is not a free SV
        fsvind = random.choice(svind)
-    anyFreeSVind += fsvind 
+    anyFreeSVind += [fsvind] 
 ## get distance
 Dist = []
-for i in range(1,len(clf14)):
-    dist = clf14[i].decision_function(TrX11)[anyFreeSVind[i]]
-    Dist += dist
+for i in range(0,len(clf14)):
+    dist = abs(clf14[i].decision_function(TrX11)[anyFreeSVind[i]])
+    Dist += [dist]
 ## plot
 plt.plot(np.log10(C), Dist)
 plt.title("Prob 14")
+plt.xlabel("log10(C)")
+plt.ylabel("distances of a free SV to hyperplane")
+plt.show()
 
 # Prob15
 for eachR in TeData:
     if eachR[0] != 1:
        eachR[0] = -1
-TeY = TeData[:,0]
+TeY = TeData[:,[0]]
 TeX = TeData[:,[1,2]]
 Cfix = 0.1
 Gamma = [1, 10, 100, 1000, 10000]
-clf15 = {}
-## Train
-for i in range(1, len(Gamma)):
-    clf15[i] = SVC(C = Cfix, kernel = 'rbf', gamma = Gamma[i])
-    clf15.fit(TrX11, TrY11)
-## Get Eout
+clf15 = []
+## train
+for i in range(0, len(Gamma)):
+    clf = SVC(C = Cfix, kernel = 'rbf', gamma = Gamma[i])
+    clf.fit(TrX11, TrY11)
+    clf15.append(clf)
+## get Eout
 Eout = []
-for eachClf in range(1, len(clf15)):
-    eout = [(clf15[i].predict(TeX[i]) != TeY[i]) for i in len(TeData)]
-    Eout += [float(sum(eout))/len(TeData)]
-## Plot
+for eachClf in range(0, len(clf15)):	
+    # original method
+    '''eout = [(clf15[i].predict(TeX[i]) != TeY[i]) for i in len(TeData)]'''
+    eout = 1-clf15[eachClf].score(TeX, TeY)
+    Eout += [eout] # original : float(sum(eout))/len(TeData)
+## plot
 plt.plot(np.log10(Gamma), Eout)
+plt.title('Prob15')
+plt.xlabel("log10(Gamma)")
+plt.ylabel("Eout")
 plt.show()
 
 # Prob16
 valRepT = 100
-for t in range(1, valRepT):
-    randSmp = 
+choosenT = [0]*5 # number of time that a given gamma is choosen  
+for t in range(0, valRepT):
+    valTrain, valTest = train_test_split(TeData,test_size=1000) 
+    valTrX = valTrain[:,[1,2]]
+    valTrY = valTrain[:,[0]]
+    valTeX = valTest[:,[1,2]]
+    valTeY = valTest[:,[0]]
+    Eval = []
+    for i in range(0, len(Gamma)):
+        clf = SVC(C = Cfix, kernel = 'rbf', gamma = Gamma[i]).fit(valTrX, valTrY)
+        eval = 1-clf.score(valTeX, valTeY) 
+        '''print(eval)'''
+        Eval += [eval]  
+    mingamInd = Eval.index(min(Eval))
+    '''print(mingamInd)'''
+    choosenT[mingamInd] += 1 
+    '''print(t)'''
+## Plot
+plt.bar(np.log10(Gamma), choosenT)
+plt.title('Prob16')
+plt.xlabel("log10(Gamma)")
+plt.ylabel("selected time of a given gamma")
+plt.show()
